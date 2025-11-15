3a3ded76de04eece1971914809b5b03a7137698d
Ansal James <ansaljames45@gmail.com>
2025-11-15 21:12:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ArithmeticOperations
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the First Number : ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the Second Number : ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($" In Addition : {num1 + num2}");
+        Console.WriteLine($" In Subtraction : {num1 - num2}");
+        Console.WriteLine($" In Division : {num1 / num2}");
+        Console.WriteLine($" In Multiplication : {num1 * num2}");
+
+    }
+}
